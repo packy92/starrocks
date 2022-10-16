a31e6f34d514d1ee5801894ed4e19b12f775c955
packy <wangchao@starrocks.com>
2022-10-16 21:53:59 +0800

    test

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/ExpressionContext.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/ExpressionContext.java
@@ -75,7 +75,7 @@ public class ExpressionContext {
         return groupExpression.arity();
     }
 
-    public GroupExpression getGroupExpression() {return groupExpression;}
+    public GroupExpression getGroupExpression() {return groupExpression; }
 
     public LogicalProperty getRootProperty() {
         return rootProperty;
